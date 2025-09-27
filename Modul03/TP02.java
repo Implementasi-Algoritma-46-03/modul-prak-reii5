0f53835801a111d7926aca2d344533d3a4f8220d
Sofian Triadi <gsofian153@gmail.com>
2025-09-27 18:37:53 +0700

    commit 01

--- Modul03/TP02.java
+++ Modul03/TP02.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class TP02 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        String Nama;
+        System.out.print("Masukkan Nama: ");
+        Nama = input.nextLine();
+
+        int Jumlah_Barang;
+        System.out.print("Masukan jumlah barang: ");
+        Jumlah_Barang = input.nextInt();
+
+        int Harga_perbarang;
+        System.out.print("Masukkan harga perbarang: ");
+        Harga_perbarang = input.nextInt();
+
+        int Total_harga = Jumlah_Barang * Harga_perbarang;
+
+        System.out.print("Hi, " + Nama + ". Total belanja adalah: " + Total_harga + " rupiah.");
     }
 }
--- Modul03/TP03.java
+++ Modul03/TP03.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class TP03 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        int Jumlah_permen;
+        System.out.print("Masukkan Jumlah permen: ");
+        Jumlah_permen = input.nextInt();
+
+        int Jumlah_teman;
+        System.out.print("Masukkan Jumlah teman: ");
+        Jumlah_teman = input.nextInt();
+
+        int permen_perteman = Jumlah_permen / (Jumlah_teman + 1);
+        int sisa_permen = Jumlah_permen % (Jumlah_teman + 1);
+        if (sisa_permen == 0) {
+            System.out.print("jumlah permen ada " + Jumlah_permen + ", dibagi ke Dira dan " + Jumlah_teman + " temannya, maka setiap orang mendapat " + permen_perteman + " perteman, tanpa ada sisa.");
+        } else {
+            System.out.print("jumlah permen ada " + Jumlah_permen + ", dibagi ke Dira dan " + Jumlah_teman + " temannya, maka setiap orang mendapat " + permen_perteman + " perteman, bersisa " + sisa_permen + " buah.");
+        }
     }
 }
