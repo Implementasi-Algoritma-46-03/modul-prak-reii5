597489c714f3bb09e8ea8d066d8108c6284cf763
Sofian Triadi <gsofian153@gmail.com>
2025-09-29 22:13:35 +0700

    hilangkan basa basi.

--- Modul03/TP02.java
+++ Modul03/TP02.java
@@ -6,20 +6,12 @@ public class TP02 {
         // Kerjakan soalnya di sini
         Scanner input = new Scanner(System.in);
 
-        String Nama;
-        System.out.print("Masukkan Nama: ");
-        Nama = input.nextLine();
-
-        int Jumlah_Barang;
-        System.out.print("Masukan jumlah barang: ");
-        Jumlah_Barang = input.nextInt();
-
-        int Harga_perbarang;
-        System.out.print("Masukkan harga perbarang: ");
-        Harga_perbarang = input.nextInt();
+        String nama = input.nextLine();
+        int Jumlah_Barang = input.nextInt();
+        int Harga_perbarang = input.nextInt();
 
         int Total_harga = Jumlah_Barang * Harga_perbarang;
 
-        System.out.print("Hi, " + Nama + ". Total belanja adalah " + Total_harga + " rupiah.");
+        System.out.print("Hi, " + nama + ". Total belanja adalah " + Total_harga + " rupiah.");
     }
 }
