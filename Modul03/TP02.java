02d99f182b464609e42f470d8c4fd4d44dce5065
Sofian Triadi <gsofian153@gmail.com>
2025-09-29 22:24:28 +0700

    commit mengganti println()

--- Modul03/TP02.java
+++ Modul03/TP02.java
@@ -6,12 +6,12 @@ public class TP02 {
         // Kerjakan soalnya di sini
         Scanner input = new Scanner(System.in);
 
-        String nama = input.nextLine();
+        String namaPembeli = input.nextLine();
         int Jumlah_Barang = input.nextInt();
         int Harga_perbarang = input.nextInt();
 
         int Total_harga = Jumlah_Barang * Harga_perbarang;
 
-        System.out.print("Hi, " + nama + ". Total belanja adalah " + Total_harga + " rupiah.");
+        System.out.println("Hi, " + namaPembeli + ". Total belanja adalah " + Total_harga + " rupiah.");
     }
 }
