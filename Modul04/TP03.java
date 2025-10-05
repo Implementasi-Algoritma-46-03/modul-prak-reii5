6fc834ed2cd33714580b5efcad7e7e8b3046ba8a
Sofian Triadi <gsofian153@gmail.com>
2025-10-05 12:16:50 +0700

    commit tp

--- Modul04/TP03.java
+++ Modul04/TP03.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class TP03 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+        int nilaiDira = input.nextInt();
+        int nilaiRadi = input.nextInt();
+        int nilaiIdar = input.nextInt();
+
+        if (nilaiDira > nilaiRadi && nilaiDira > nilaiIdar) {
+            System.out.println("Dira");
+        } else if (nilaiRadi > nilaiDira && nilaiRadi > nilaiIdar) {
+            System.out.println("Radi");
+        } else if (nilaiIdar > nilaiDira && nilaiIdar > nilaiRadi) {
+            System.out.println("Idar");
+        }
+
+
     }
 }
