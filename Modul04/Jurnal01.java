5cdd3c2fe94283bd3aea00ffb181fb32ce33cd1a
Sofian Triadi <gsofian153@gmail.com>
2025-10-09 17:02:03 +0700

    commit jurnal

--- Modul04/Jurnal01.java
+++ Modul04/Jurnal01.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Jurnal01 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner in = new Scanner(System.in);
+        int bilangan = in.nextInt();
+
+        if (24 % bilangan == 0 && 56 % bilangan == 0){
+            System.out.println("Faktor dari 24 dan 56");
+        }else{
+            System.out.println("Bukan faktor dari 24 dan 56");
+        }
     }
 }
