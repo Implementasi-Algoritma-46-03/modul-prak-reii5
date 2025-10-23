f2412d6a744f45e1224f189d8b9223d3b3e2525a
Sofian Triadi <gsofian153@gmail.com>
2025-10-23 16:52:49 +0700

    jurnal03

--- Modul06/Jurnal03.java
+++ Modul06/Jurnal03.java
@@ -6,14 +6,11 @@ public class Jurnal03 {
         return volume;
     }
     public static void persentase(int tinggiAir, int jari_jari, int tinggi){
+        double volume = volume(jari_jari, tinggi);
         double air = 3.14159 * ((double) jari_jari * jari_jari) * tinggiAir;
         double persentase =air / volume(jari_jari, tinggi) * 100;
-        System.out.printf("%.2f", volume(jari_jari, tinggi));
-        System.out.println();
-        System.out.printf("%.2f", air);
-        System.out.println();
-        System.out.printf("%.1f", persentase);
-        System.out.println("%");
+        System.out.printf("%.2f %.2f %.1f%%", volume, air, persentase);
+
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
