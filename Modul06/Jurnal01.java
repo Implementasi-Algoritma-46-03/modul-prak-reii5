d021818bf77a568d555cf54f82dd3494b89b09a7
Sofian Triadi <gsofian153@gmail.com>
2025-10-23 16:39:52 +0700

    jurnal

--- Modul06/Jurnal01.java
+++ Modul06/Jurnal01.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
 public class Jurnal01 {
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+
+public static void sout(String nama1, String nama2, int waktu){
+    System.out.println("Halo, " + nama1 + ". Selamat " + sapa(waktu) + ".");
+    System.out.println("Halo, " + nama2 + ". Selamat " + sapa(waktu) + ".");
+}
+  
+public static String sapa(int waktu){  
+    if (waktu >= 06 && waktu <= 11){ 
+        return "pagi";
+    }else if (waktu >= 12 && waktu <= 14){
+        return "siang";
+    }else if (waktu >= 15 && waktu <= 17){
+        return "sore";
+    }else{
+        return "malam";
     }
+}
+
+public static void main(final String[] args) {
+    Scanner in = new Scanner(System.in);
+    String nama1 = in.nextLine();
+    String nama2 = in.nextLine();
+    int waktu = in.nextInt();
+
+    sout(nama1, nama2, waktu);
+}
 }
--- Modul06/Jurnal03.java
+++ Modul06/Jurnal03.java
@@ -1,6 +1,28 @@
-public class Jurnal03 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal03 {
+    public static double volume(int jari_jari, int tinggi){
+        double volume = 3.14159 * ((double) jari_jari * jari_jari) * tinggi;
+        return volume;
     }
+    public static void persentase(int tinggiAir, int jari_jari, int tinggi){
+        double air = 3.14159 * ((double) jari_jari * jari_jari) * tinggiAir;
+        double persentase =air / volume(jari_jari, tinggi) * 100;
+        System.out.printf("%.2f", volume(jari_jari, tinggi));
+        System.out.println();
+        System.out.printf("%.2f", air);
+        System.out.println();
+        System.out.printf("%.1f", persentase);
+        System.out.println("%");
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int jari_jari = in.nextInt();
+        int tinggi = in.nextInt();
+        int tinggiAir = in.nextInt();;
+
+        persentase(tinggiAir, jari_jari, tinggi);
+
+    }
+    
 }
