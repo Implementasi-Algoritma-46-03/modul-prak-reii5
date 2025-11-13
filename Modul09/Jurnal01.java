8dcfc9f7c32c98d481f1f1acfe226865ce0cb2d9
Sofian <gsofian153@gmail.com>
2025-11-13 16:26:53 +0700

    duh jurnal 1 salah tadi

--- Modul09/Jurnal01.java
+++ Modul09/Jurnal01.java
@@ -1,10 +1,10 @@
 public class Jurnal01 {
-
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
-        for (int i = 1; i <= 10; i++){
-            System.out.println(i * i + "");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i * i);
+            if (i < 10) {
+                System.out.print(" ");
+            }
         }
-    } 
+    }
 }
- 
