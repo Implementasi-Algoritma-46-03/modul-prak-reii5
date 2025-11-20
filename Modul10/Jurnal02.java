a780be49a300eb0be2ebed23eb99226ae36087dc
Sofian <gsofian153@gmail.com>
2025-11-20 15:52:10 +0700

    ngumpulin jurnal

--- Modul10/Jurnal02.java
+++ Modul10/Jurnal02.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Jurnal02 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner in = new Scanner(System.in);
+
+        int masukan = in.nextInt();
+
+        for (int i = 1; i <= masukan; i++){
+
+            for (int j = 1; j <= masukan; j++){
+                System.out.printf("%-4d", i *j);
+            }
+            System.out.println();
+        }
     }
 }
--- Modul10/Jurnal03.java
+++ Modul10/Jurnal03.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Jurnal03 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner in = new Scanner(System.in);
+
+        int masukan = in.nextInt();
+        int s = 0;
+        for (int i = masukan; i >= 1; i--){
+
+            for(int j = 0; j < i; j++) {               
+                System.out.print(s);
+                s++;
+                if(s > 9){
+                    s = 0;
+                }
+            }
+            System.out.println();
+        }
     }
 }
