fa4924dc2e6da4e39433b164a0a9c43f1ac6e8da
Sofian <gsofian153@gmail.com>
2025-11-26 08:09:12 +0700

    tp02 tadi salah

--- Modul11/TP02.java
+++ Modul11/TP02.java
@@ -3,19 +3,24 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class TP02 {
-    
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-
+        if (!s.hasNextInt()) return;          // tidak ada input sama sekali
         int N = s.nextInt();
         Integer[] poin = new Integer[N];
-
-        for (int i = 0; i < N; i++) {
-            poin[i] = s.nextInt();
+        int count = 0;
+        while (count < N && s.hasNextInt()) { // baca sampai N atau sampai input habis
+            poin[count++] = s.nextInt();
         }
+        // jika input kurang dari N, isi sisa dengan 0 (atau keputusan lain sesuai soal)
+        for (int i = count; i < N; i++) poin[i] = 0;
 
         Arrays.sort(poin, Collections.reverseOrder());
 
         for (int i = 0; i < N; i++) {
-            System.out.print(poin[i] + " ");
-        }}}
+            System.out.print(poin[i]);
+            if (i < N - 1) System.out.print(" ");
+        }
+        s.close();
+    }
+}
