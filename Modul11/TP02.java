362cc087a3da3c73cdbac81fbf3705fc24809171
Sofian <gsofian153@gmail.com>
2025-11-27 16:44:58 +0700

    coba tp

--- Modul11/TP02.java
+++ Modul11/TP02.java
@@ -1,26 +1,30 @@
 import java.util.Scanner;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class TP02 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        if (!s.hasNextInt()) return;          // tidak ada input sama sekali
-        int N = s.nextInt();
-        Integer[] poin = new Integer[N];
-        int count = 0;
-        while (count < N && s.hasNextInt()) { // baca sampai N atau sampai input habis
-            poin[count++] = s.nextInt();
-        }
-        // jika input kurang dari N, isi sisa dengan 0 (atau keputusan lain sesuai soal)
-        for (int i = count; i < N; i++) poin[i] = 0;
 
-        Arrays.sort(poin, Collections.reverseOrder());
+        int N = s.nextInt();
+        int[] poin = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            poin[i] = s.nextInt();
+        }
+
+        Arrays.sort(poin); 
+
+   
+        for (int i = 0; i < N / 2; i++) {
+            int tmp = poin[i];
+            poin[i] = poin[N - 1 - i];
+            poin[N - 1 - i] = tmp;
+        }
 
         for (int i = 0; i < N; i++) {
             System.out.print(poin[i]);
             if (i < N - 1) System.out.print(" ");
         }
-        s.close();
+
     }
 }
