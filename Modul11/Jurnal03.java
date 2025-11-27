9acc11cbe74a918ec39d42f69787b9de4ea78deb
Sofian <gsofian153@gmail.com>
2025-11-27 10:28:44 +0700

    jurnal 3 perbaikan

--- Modul11/Jurnal03.java
+++ Modul11/Jurnal03.java
@@ -4,7 +4,7 @@ public class Jurnal03 {
 
     public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
-        int counter = 0;
+        int counter;
         int hasilAkhir = 0;
         int a;
         int modus = 0;
@@ -18,13 +18,15 @@ public class Jurnal03 {
         } while (a != -1);
 
         for (int i=0; i<arr.size(); i++) {
+            counter = 0;
             for (int j=0; j<arr.size(); j++) {
-                if (arr.get(i) == arr.get(j));
+                if (arr.get(i).equals(arr.get(j))){
                     counter++;
+                }
             }
-            if (counter >= hasilAkhir) {
-                modus = arr.get(i);
+            if (counter > hasilAkhir || (counter == hasilAkhir && arr.get(i) > modus)) {
                 hasilAkhir = counter;
+                modus = arr.get(i);
             }
         }
         System.out.println(modus);
