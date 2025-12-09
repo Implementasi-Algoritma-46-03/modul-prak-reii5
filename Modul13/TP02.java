7d4ce7affc2170b5404d1eb490be8efee22e6429
Sofian <gsofian153@gmail.com>
2025-12-10 06:13:36 +0700

    commit tp

--- Modul13/TP02.java
+++ Modul13/TP02.java
@@ -1,6 +1,43 @@
-public class TP02 {
+import java.util.Scanner;
+import java.util.ArrayList;
 
+public class TP02 {
+    public static float[][] hasilSortng(float[][] data){
+        for (int i = 0; i< 2; i++){
+            for(int j = 1; j < 10;j++){
+                for(int k = 10-1; k>=1;k--){
+                    if  (data[i][k] > data[i][k - 1]){
+                        float temp = data[i][k];
+                        data[i][k] = data[i][k -1];
+                        data[i][k - 1] = temp;
+                    }
+                }
+            }
+        }
+        return data;
+    }
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner in = new Scanner(System.in);
+
+        float[][] data = new float[2][10];
+
+        for (int i =  0; i < 2; i++){
+            for (int j = 0;j < 10; j++){
+                data[i][j] = in.nextFloat();
+            }
+        }
+
+        float[][] hasil = hasilSortng(data);
+
+        for(int i = 0; i <2; i++){
+            for (int j = 0; j < 10;j++){
+                System.out.printf("%.2f",hasil[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        
+
     }
 }
--- Modul13/TP03.java
+++ Modul13/TP03.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class TP03 {
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+    public static int linearSearch(int[] data, int key) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int jumlah = in.nextInt();
+        int[] data = new int[jumlah];
+
+        for (int i = 0; i < jumlah; i++) {
+            data[i] = in.nextInt();
+        }
+
+        int key = in.nextInt();
+
+        int hasil = linearSearch(data, key);
+
+        if (hasil != -1) {
+            System.out.println("Ditemukan di indeks ke-" + hasil);
+        } else {
+            System.out.println("Tidak ditemukan");
+        }
     }
 }
