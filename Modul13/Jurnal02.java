7938d886798a565248eaaee275b71ee662e95498
Sofian <gsofian153@gmail.com>
2025-12-11 17:15:52 +0700

    commit jurnal

--- Modul13/Jurnal02.java
+++ Modul13/Jurnal02.java
@@ -1,6 +1,30 @@
-public class Jurnal02 {
+import java.util.*;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal02 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int masukan = in.nextInt();
+        String[] data = new String[masukan];
+
+        for (int i = 0; i < masukan; i++) {
+         data[i] = in.next();
+        }
+
+        for (int i = 1; i < masukan; i++) {
+            String teks = data[i];
+            int indeks = i - 1;
+
+            while (indeks >= 0 && data[indeks].compareTo(teks) > 0) {
+             data[indeks + 1] = data[indeks];
+                indeks--;
+            }
+         data[indeks + 1] = teks;
+        }
+
+        for (int i = 0; i < masukan; i++) {
+            System.out.print(data[i]);
+            if (i < masukan - 1) System.out.print(" ");
+        }
     }
 }
--- Modul13/Jurnal03.java
+++ Modul13/Jurnal03.java
@@ -1,6 +1,57 @@
+import java.util.Scanner;
+
 public class Jurnal03 {
 
+    public static void binarySearch(String[] data, String target) {
+        int low = 0;
+        int high = data.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (data[mid].equals(target)) {
+                System.out.println("Ditemukan di indeks ke-" + mid);
+                return;
+            }
+
+            if (target.compareTo(data[mid]) < 0) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        System.out.println("Data tidak ditemukan");
+    }
+
+    public static void sort(String[] data, int n) {
+        for (int i = 1; i < n; i++) {
+            String key = data[i];
+            int j = i - 1;
+
+            while (j >= 0 && data[j].compareTo(key) > 0) {
+                data[j+1] = data[j];
+                j--;
+            }
+            data[j+1] = key;
+        }
+    }
+
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        in.nextLine(); // buang newline
+
+        String[] data = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            data[i] = in.next();
+        }
+
+        String search = in.next();
+
+        sort(data, n);
+        binarySearch(data, search);
     }
 }
